c297461ba7d12733723bbb651d4648a2e0444021
prince-bhalala <princebhalala24@gmail.com>
2025-05-27 18:44:41 +0530

    feat: implement user sign-up API with email verification
    - Added a new route for user sign-up that handles user registration and email verification.
    - Integrated bcryptjs for password hashing.
    - Created a helper function to send verification emails using Resend.
    - Added a new model for API response structure.
    - Updated package.json to include necessary dependencies for email handling and bcrypt.

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,95 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User.model";
+import bcrypt from "bcryptjs";
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+import { success } from "zod/v4";
+
+export async function POST(request : Request){
+    await dbConnect()
+    try {
+
+        const {username,email,password} = await request.json()
+        
+        const existingUserVerifiedByUsername = await UserModel.findOne({
+            username , 
+            isVerified : true
+        })
+
+        if (existingUserVerifiedByUsername) {
+            return Response.json({
+                success : false,
+                message : "Username is allready taken"
+            } , { status : 400 })
+        }
+
+        const existingUserByEmail  = await UserModel.findOne({email})
+
+        const verifyCode = Math.floor(100000 + Math.random() * 900000).toString()
+
+        if (existingUserByEmail) {
+            if (existingUserByEmail.isVerified) {
+                return Response.json({
+                success : false,
+                message : "User already exist with this email"
+            } , { status : 400 })
+            }
+            else {
+                const hasedPassword = await bcrypt.hash(password , 10)
+                existingUserByEmail.password = hasedPassword;
+                existingUserByEmail.verifyCode = verifyCode ; 
+                existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000)
+                await existingUserByEmail.save()
+            }
+        }
+        else {
+            const hasedPassword = await bcrypt.hash(password , 10)
+            const expiryDate = new Date()
+            expiryDate.setHours(expiryDate.getHours() + 1)
+
+           const newUser =  new UserModel({
+                    username ,
+                    email ,
+                    password : hasedPassword,
+                    verifyCode ,
+                    verifyCodeExpiry : expiryDate ,
+                    isVerified : false ,
+                    isAcceptingMessage : true,
+                    messages : []
+            })
+
+            await newUser.save()
+
+        }
+
+        // send verifcation email
+
+        const emaileResponse = await sendVerificationEmail(
+            email ,
+            username,
+            verifyCode
+        )
+
+        if (!emaileResponse.success) {
+            return Response.json({
+                success : false,
+                message : emaileResponse.message
+            } , { status : 500 })
+        }
+
+        return Response.json({
+                success : true,
+                message : "User registerd successfully. Please verify your email"
+            } , { status : 200 })
+
+    } catch (error) {
+
+        console.error("Error Registring user",error)
+        return Response.json({
+            success : false,
+            message : "Error registring user"
+        },
+        {
+            status : 500
+        })
+    }
+}
---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,26 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificatioEmail"
+import { ApiResponse } from "@/types/ApiResponsa";
+import { fa } from "zod/v4/locales";
+
+
+export async function sendVerificationEmail (
+    email : string,
+    username : string,
+    verifyCode : string
+) : Promise<ApiResponse> {
+    try {
+        await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: email,
+            subject: 'Verification code',
+            react: VerificationEmail({username , otp : verifyCode}),
+    });
+        return {success : true , message : "verification email send succesfully"}
+        
+    } catch (emailError) {
+        console.error("Error sending verification email",emailError)
+        return {success : false , message : "failed to send verification email"}
+        
+    }
+}
